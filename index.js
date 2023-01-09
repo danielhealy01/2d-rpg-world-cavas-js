cf645a1ebe58ca83c14d31c7d05de2605aee3a21
DanielHealy <danielhealyhosting3@gmail.com>
2023-01-09 12:28:34 +0000

    sprite class built, background render refactored as a class method, player movement instantiated

--- index.js
+++ index.js
@@ -15,12 +15,45 @@ image.src = './assets/Pellet Town.png';
 const playerImage = new Image();
 playerImage.src = './assets/playerDown.png';
 
+class Sprite {
+	constructor({ position, velocity, image }) {
+		// pass in params as obj so order of params don't matter
+		this.position = position;
+		this.image = image;
+	}
+	draw() {
+		c.drawImage(this.image, this.position.x, this.position.y);
+	}
+}
+
+const background = new Sprite({
+	position: {
+		x: -785,
+		y: -650,
+	},
+	image: image,
+});
+
+const keys = {
+	w: {
+		pressed: false,
+	},
+	a: {
+		pressed: false,
+	},
+	s: {
+		pressed: false,
+	},
+	d: {
+		pressed: false,
+	},
+};
+
 //animation loop - infintie recursion, repaints window at the display refresh rate
 function animate() {
 	window.requestAnimationFrame(animate);
+	background.draw();
 	console.log('animate');
-
-	c.drawImage(image, -785, -650);
 	c.drawImage(
 		//src
 		//crop x start
@@ -44,22 +77,50 @@ function animate() {
 		playerImage.width / 4,
 		playerImage.height
 	);
+	if (keys.s.pressed && lastKey === 's') background.position.y -= 3;
+	else if (keys.w.pressed && lastKey === 'w') background.position.y += 3;
+	else if (keys.a.pressed && lastKey === 'a') background.position.x += 3;
+	else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3;
 }
 animate();
 
+let lastKey = '';
 window.addEventListener('keydown', (e) => {
 	switch (e.key) {
 		case 'w':
-			console.log('w');
+			keys.w.pressed = true;
+			console.log(keys);
+			lastKey = 'w';
 			break;
 		case 'a':
-			console.log('a');
+			keys.a.pressed = true;
+			lastKey = 'a';
 			break;
 		case 's':
-			console.log('s');
+			keys.s.pressed = true;
+			lastKey = 's';
 			break;
 		case 'd':
-			console.log('d');
+			keys.d.pressed = true;
+			lastKey = 'd';
+			break;
+	}
+});
+
+window.addEventListener('keyup', (e) => {
+	switch (e.key) {
+		case 'w':
+			keys.w.pressed = false;
+			console.log(keys);
+			break;
+		case 'a':
+			keys.a.pressed = false;
+			break;
+		case 's':
+			keys.s.pressed = false;
+			break;
+		case 'd':
+			keys.d.pressed = false;
 			break;
 	}
 });
